c39ada768c6e8f230318f9ba3fca8aa16bfae751
koyeb01125 <apnapremiummarket011@gmail.com>
2025-02-18 01:23:32 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -92,7 +92,7 @@ help_pages = [
         "> 4. REPLACEWORDS : Can be used for words in deleted set via REMOVE WORDS\n"
         "> 5. RESET : To set the things back to default\n\n"
         "> You can set CUSTOM THUMBNAIL, PDF WATERMARK, VIDEO WATERMARK, SESSION-based login, etc. from settings\n\n"
-        "**__Powered by @Kingofmarket03__**"
+        "**__Powered by @Adityaseller02__**"
     )
 ]
 
@@ -166,7 +166,7 @@ async def terms(client, message):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/Kingofmarket03")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/Adityaseller02")],
         ]
     )
     await message.reply_text(terms_text, reply_markup=buttons)
@@ -175,7 +175,7 @@ async def terms(client, message):
 @app.on_message(filters.command("plan") & filters.private)
 async def plan(client, message):
     plan_text = (
-        "💰 **Premium Price**: Starting from  499rs for 1 months 😇accepted via **Upi** to purchase contact @Kingofmarket03 (terms and conditions apply).\n"
+        "💰 **Premium Price**: Starting from  199rs for 1 months 😇accepted via **Upi** to purchase contact @Adityaseller02 (terms and conditions apply).\n"
         "📥 **Download Limit**: Users can download up to 10,00,000 files in a single batch command.\n"
         "🛑 **Batch**: You will get two modes /bulk and /batch.\n"
         "   - Users are advised to wait for the process to automatically cancel before proceeding with any downloads or uploads.\n\n"
@@ -185,7 +185,7 @@ async def plan(client, message):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/Kingofmarket03")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/Adityaseller02")],
         ]
     )
     await message.reply_text(plan_text, reply_markup=buttons)
@@ -194,7 +194,7 @@ async def plan(client, message):
 @app.on_callback_query(filters.regex("see_plan"))
 async def see_plan(client, callback_query):
     plan_text = (
-       "💰 **Premium Price**: Starting from 199rs for 1 months accepted via **Upi** to purchase contact @Personal123premium (terms and conditions apply).\n"
+       "💰 **Premium Price**: Starting from 199rs for 1 months accepted via **Upi** to purchase contact @Adityaseller02 (terms and conditions apply).\n"
         "📥 **Download Limit**: Users can download up to 10,00,000 files in a single batch command.\n"
         "🛑 **Batch**: You will get two modes /bulk and /batch.\n"
         "   - Users are advised to wait for the process to automatically cancel before proceeding with any downloads or uploads.\n\n"
@@ -204,7 +204,7 @@ async def see_plan(client, callback_query):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/Kingofmarket03")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/Adityaseller02")],
         ]
     )
     await callback_query.message.edit_text(plan_text, reply_markup=buttons)
@@ -222,7 +222,7 @@ async def see_terms(client, callback_query):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/Kingofmarket03")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/Adityaseller02")],
         ]
     )
     await callback_query.message.edit_text(terms_text, reply_markup=buttons)
